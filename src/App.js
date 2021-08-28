2f676bd8eea01c700d825b530a33b0c7cfa4a9fe
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-08-28 16:08:05 -0300

    Created structure for JSX

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 // Imports
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import data from "./api";
 
 // Component
 const App = () => {
+  // Hooks
+  const [movieList, setMovieList] = useState([]);
+
   // Methods
   useEffect(() => {
     const loadData = async () => {
       let movieList = await data.getHomeList();
-      console.log(movieList);
+      setMovieList(movieList);
     };
 
     loadData();
   }, []);
 
-  return <div>Hello, World</div>;
+  // JSX
+  return (
+    <div className="page">
+      {/* Header */}
+      <div className="header"></div>
+
+      {/* Featured movie */}
+      <div className="featured"></div>
+
+      {/* Lists of movies */}
+      <div className="lists"></div>
+
+      {/* Footer */}
+      <div className="footer"></div>
+    </div>
+  );
 };
 
 // Export
