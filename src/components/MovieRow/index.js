a59798d3a778a8f224b32e1bfee17c9670600b7c
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-08-29 16:35:25 -0300

    Added movement to movelist when arrow is clicked

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,17 +1,22 @@
 // Imports
 import "./MovieRow.css";
 import { useState, useEffect } from "react";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 
 // Component
 const MovieRow = ({ title, list }) => {
   // Hooks
   const [movieList, setMovieList] = useState(list);
+  const [scrollX, setScrollX] = useState(-400);
 
   // Methods
   useEffect(() => {
+    console.log(movieList.results);
     const shuffleList = (unshuffledList) => {
       for (let i = unshuffledList.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
+
         [unshuffledList[i], unshuffledList[j]] = [
           unshuffledList[j],
           unshuffledList[i],
@@ -24,12 +29,47 @@ const MovieRow = ({ title, list }) => {
     setMovieList(shuffleList(movieList.results));
   }, []);
 
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2);
+
+    if (x > 0) {
+      x = 0;
+    }
+
+    setScrollX(x);
+  };
+
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let listW = list.results.length * 150;
+
+    if (window.innerWidth - listW > x) {
+      x = window.innerWidth - listW - 60;
+    }
+
+    setScrollX(x);
+  };
+
   // JSX
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+
+      <div className="movieRow__left" onClick={handleLeftArrow}>
+        <NavigateBeforeIcon style={{ fontSize: 50 }} />
+      </div>
+      <div className="movieRow__right" onClick={handleRightArrow}>
+        <NavigateNextIcon style={{ fontSize: 50 }} />
+      </div>
+
       <div className="movieRow__listarea">
-        <div className="movieRow__list">
+        <div
+          className="movieRow__list"
+          style={{
+            marginLeft: scrollX,
+            width: list.results.length * 150,
+          }}
+        >
           {list.results.length > 0 &&
             list.results.map((movie, key) => {
               return (
