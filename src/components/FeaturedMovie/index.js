bb4aeb194ed38616d91bd786d68849689238141b
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-08-29 09:57:29 -0300

    Created FeaturedMovie component

---
+++ src/components/FeaturedMovie/index.js
@@ -0,0 +1,12 @@
+// Imports
+import "./FeaturedMovie.css";
+
+const FeaturedMovie = () => {
+  return (
+    <div>
+      <h1>Featured Movie here</h1>
+    </div>
+  );
+};
+
+export default FeaturedMovie;
