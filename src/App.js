cac0b05998023f5f7085c127ebd16a70571e6639
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-08-29 10:22:47 -0300

    Got info about featured movie

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import data from "./api";
 import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
 
 // Component
 const App = () => {
   // Hooks
   const [movieList, setMovieList] = useState([]);
+  const [featuredMovie, setFeaturedMovie] = useState(null);
 
   // Methods
   useEffect(() => {
     const loadData = async () => {
+      // List of movies
       let movieList = await data.getHomeList();
       setMovieList(movieList);
+
+      // Featured movie
+      let netflixOriginals = movieList.filter(
+        (type) => type.camel === "originals"
+      );
+      let randomNumber = Math.floor(
+        Math.random() * netflixOriginals[0].items.results.length - 1
+      );
+      let chosenFeatured = netflixOriginals[0].items.results[randomNumber];
+      let chosenInfo = await data.getMovieInfo(chosenFeatured.id, "tv");
+      setFeaturedMovie(chosenInfo);
     };
 
     loadData();
@@ -26,7 +40,9 @@ const App = () => {
       <section className="header"></section>
 
       {/* Featured movie */}
-      <section className="featured"></section>
+      <section className="featured">
+        {featuredMovie && <FeaturedMovie featuredData={featuredMovie} />}
+      </section>
 
       {/* Lists of movies */}
       <section className="lists">
