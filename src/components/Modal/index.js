590a77ae7d4563266b57f263c87187e0dd823ba4
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-08-31 17:09:49 -0300

    Modal is almost finished

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,12 +1,19 @@
 import "./Modal.css";
 
 const Modal = (props) => {
-  console.log(props.modalInfo);
+  let genres = [];
+  if (props.modalInfo.genres) {
+    for (let genre of props.modalInfo.genres) {
+      genres.push(genre.name);
+    }
+
+    genres = genres.join(", ");
+  }
 
   return (
     <div className="modal" onClick={() => props.setModal(false)}>
       {props.showType === "tv" ? (
-        <div className="modal__card">
+        <div className="modal__card" onClick={() => props.setModal(true)}>
           <div
             className="modal__image"
             style={{
@@ -19,6 +26,34 @@ const Modal = (props) => {
               <a href="/#" className="modal__button">
                 Play
               </a>
+            </div>
+          </div>
+          <div className="modal__info">
+            <div className="modal__details">
+              <h3 className="modal__rating">
+                {Math.round(props.modalInfo.vote_average * 10)}% match
+              </h3>
+              <h3 className="modal__year">
+                {props.modalInfo.first_air_date
+                  ? props.modalInfo.first_air_date.substring(0, 4)
+                  : null}
+              </h3>
+              <h3 className="modal__seasons">
+                {props.modalInfo.number_of_seasons} season
+                {props.modalInfo.number_of_seasons !== 1 ? "s" : ""}
+              </h3>
+            </div>
+            <div className="modal__description">
+              <p>
+                {props.modalInfo.overview ? (
+                  `${props.modalInfo.overview}`
+                ) : (
+                  <span className="modal__no-description">
+                    No overview for this show
+                  </span>
+                )}
+              </p>
+              <p>Genres: {genres}</p>
             </div>
           </div>
         </div>
