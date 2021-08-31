a6ceac7d3a9cbd29cf81996212493070bc2b8b14
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-08-31 14:56:58 -0300

    Modal now shows movie backdrop and title

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,13 +1,44 @@
 import "./Modal.css";
 
 const Modal = (props) => {
-  console.log(props);
+  console.log(props.modalInfo);
+
   return (
     <div className="modal">
       {props.showType === "tv" ? (
-        <div className="modal__info">{props.modalInfo.name}</div>
+        <div className="modal__card">
+          <div
+            className="modal__image"
+            style={{
+              backgroundImage: `linear-gradient(to top, rgba(24, 24, 24), transparent 50%), url(
+                https://image.tmdb.org/t/p/original${props.modalInfo.backdrop_path})`,
+            }}
+          >
+            <div className="modal__title-container">
+              <h2 className="modal__title">{props.modalInfo.name}</h2>
+              <a href="/#" className="featured__buttons--red">
+                Play
+              </a>
+            </div>
+          </div>
+        </div>
       ) : (
-        <div className="modal__info">{props.modalInfo.title}</div>
+        <div className="modal__card">
+          <div
+            className="modal__image"
+            style={{
+              backgroundImage: `linear-gradient(to top, rgba(24, 24, 24), transparent 50%), url(
+                https://image.tmdb.org/t/p/original${props.modalInfo.backdrop_path})`,
+            }}
+          >
+            <div className="modal__title-container">
+              <h2 className="modal__title">{props.modalInfo.title}</h2>
+              <a href="/#" className="featured__buttons--red">
+                Play
+              </a>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
