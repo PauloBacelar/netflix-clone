dd171180cf69d0d4e00d293c5a70201a12571ef2
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-27 07:21:30 -0300

    Removed ternary operators

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -18,12 +18,10 @@ const Modal = (props) => {
           <div
             className="modal__image"
             style={
-              props.modalInfo.backdrop_path
-                ? {
-                    backgroundImage: `linear-gradient(to top, rgba(24, 24, 24), transparent 50%), url(
+              props.modalInfo.backdrop_path && {
+                backgroundImage: `linear-gradient(to top, rgba(24, 24, 24), transparent 50%), url(
                 https://image.tmdb.org/t/p/original${props.modalInfo.backdrop_path})`,
-                  }
-                : null
+              }
             }
           >
             <div className="modal__close" onClick={() => props.setModal(false)}>
@@ -44,9 +42,8 @@ const Modal = (props) => {
                 {Math.round(props.modalInfo.vote_average * 10)}% match
               </h3>
               <h3 className="modal__year">
-                {props.modalInfo.first_air_date
-                  ? props.modalInfo.first_air_date.substring(0, 4)
-                  : null}
+                {props.modalInfo.first_air_date &&
+                  props.modalInfo.first_air_date.substring(0, 4)}
               </h3>
               <h3 className="modal__seasons">
                 {props.modalInfo.number_of_seasons} season
@@ -72,58 +69,50 @@ const Modal = (props) => {
             <h2>Similar shows</h2>
 
             <div className="modal__grid">
-              {props.similarTitles
-                ? props.similarTitles.results.map((title, key) => {
-                    if (title.backdrop_path) {
-                      return (
-                        <div className="modal__grid-item" key={key}>
-                          <div
-                            className="modal__grid-img"
-                            style={
-                              title.backdrop_path
-                                ? {
-                                    backgroundImage: `url(https://image.tmdb.org/t/p/original${title.backdrop_path})`,
-                                    backgroundSize: "cover",
-                                    backgroundPosition: "center",
-                                  }
-                                : null
+              {props.similarTitles &&
+                props.similarTitles.results.map((title, key) => {
+                  if (title.backdrop_path) {
+                    return (
+                      <div className="modal__grid-item" key={key}>
+                        <div
+                          className="modal__grid-img"
+                          style={
+                            title.backdrop_path && {
+                              backgroundImage: `url(https://image.tmdb.org/t/p/original${title.backdrop_path})`,
+                              backgroundSize: "cover",
+                              backgroundPosition: "center",
                             }
-                          >
-                            <p className="modal__grid-year">
-                              {title.first_air_date !== undefined
-                                ? title.first_air_date.substring(0, 4)
-                                : ""}
-                            </p>
-                            <div className="modal__grid-play">
-                              <img src={playButton} alt="" />
-                            </div>
-                          </div>
-
-                          <div className="modal__grid-info">
-                            <div className="modal__grid-title">
-                              <div className="modal__rating">
-                                <h4>
-                                  {" "}
-                                  {Math.round(title.vote_average * 10)}% match
-                                </h4>
-                              </div>
-                              <h4 className="modal__grid-name">{title.name}</h4>
-                            </div>
-                            <div className="modal__grid-description">
-                              <p>
-                                {title.overview.length > 15
-                                  ? title.overview.substring(0, 140) + "..."
-                                  : title.overview}
-                              </p>
-                            </div>
+                          }
+                        >
+                          <p className="modal__grid-year">
+                            {title.first_air_date !== undefined &&
+                              title.first_air_date.substring(0, 4)}
+                          </p>
+                          <div className="modal__grid-play">
+                            <img src={playButton} alt="" />
                           </div>
                         </div>
-                      );
-                    }
 
-                    return null;
-                  })
-                : ""}
+                        <div className="modal__grid-info">
+                          <div className="modal__grid-title">
+                            <div className="modal__rating">
+                              <h4>
+                                {" "}
+                                {Math.round(title.vote_average * 10)}% match
+                              </h4>
+                            </div>
+                            <h4 className="modal__grid-name">{title.name}</h4>
+                          </div>
+                          <div className="modal__grid-description">
+                            <p>{title.overview.substring(0, 140) + "..."}</p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  }
+
+                  return null;
+                })}
             </div>
           </div>
         </div>
@@ -132,12 +121,10 @@ const Modal = (props) => {
           <div
             className="modal__image"
             style={
-              props.modalInfo.backdrop_path
-                ? {
-                    backgroundImage: `linear-gradient(to top, rgba(24, 24, 24), transparent 50%), url(
+              props.modalInfo.backdrop_path && {
+                backgroundImage: `linear-gradient(to top, rgba(24, 24, 24), transparent 50%), url(
                 https://image.tmdb.org/t/p/original${props.modalInfo.backdrop_path})`,
-                  }
-                : null
+              }
             }
           >
             <div className="modal__close" onClick={() => props.setModal(false)}>
@@ -158,9 +145,8 @@ const Modal = (props) => {
                 {Math.round(props.modalInfo.vote_average * 10)}% match
               </h3>
               <h3 className="modal__year">
-                {props.modalInfo.release_date
-                  ? props.modalInfo.release_date.substring(0, 4)
-                  : null}
+                {props.modalInfo.release_date &&
+                  props.modalInfo.release_date.substring(0, 4)}
               </h3>
               <h3 className="modal__seasons">
                 {Math.round(props.modalInfo.runtime / 60)}h{" "}
@@ -186,60 +172,50 @@ const Modal = (props) => {
             <h2>Similar shows</h2>
 
             <div className="modal__grid">
-              {props.similarTitles
-                ? props.similarTitles.results.map((title, key) => {
-                    if (title.backdrop_path) {
-                      return (
-                        <div className="modal__grid-item" key={key}>
-                          <div
-                            className="modal__grid-img"
-                            style={
-                              title.backdrop_path
-                                ? {
-                                    backgroundImage: `url(https://image.tmdb.org/t/p/original${title.backdrop_path})`,
-                                    backgroundSize: "cover",
-                                    backgroundPosition: "center",
-                                  }
-                                : null
+              {props.similarTitles &&
+                props.similarTitles.results.map((title, key) => {
+                  if (title.backdrop_path) {
+                    return (
+                      <div className="modal__grid-item" key={key}>
+                        <div
+                          className="modal__grid-img"
+                          style={
+                            title.backdrop_path && {
+                              backgroundImage: `url(https://image.tmdb.org/t/p/original${title.backdrop_path})`,
+                              backgroundSize: "cover",
+                              backgroundPosition: "center",
                             }
-                          >
-                            <p className="modal__grid-year">
-                              {title.release_date !== undefined
-                                ? title.release_date.substring(0, 4)
-                                : ""}
-                            </p>
-                            <div className="modal__grid-play">
-                              <img src={playButton} alt="" />
-                            </div>
-                          </div>
-
-                          <div className="modal__grid-info">
-                            <div className="modal__grid-title">
-                              <div className="modal__rating">
-                                <h4>
-                                  {" "}
-                                  {Math.round(title.vote_average * 10)}% match
-                                </h4>
-                              </div>
-                              <h4 className="modal__grid-name">
-                                {title.title}
-                              </h4>
-                            </div>
-                            <div className="modal__grid-description">
-                              <p>
-                                {title.overview.length > 15
-                                  ? title.overview.substring(0, 140) + "..."
-                                  : title.overview}
-                              </p>
-                            </div>
+                          }
+                        >
+                          <p className="modal__grid-year">
+                            {title.release_date !== undefined &&
+                              title.release_date.substring(0, 4)}
+                          </p>
+                          <div className="modal__grid-play">
+                            <img src={playButton} alt="" />
                           </div>
                         </div>
-                      );
-                    }
 
-                    return null;
-                  })
-                : null}
+                        <div className="modal__grid-info">
+                          <div className="modal__grid-title">
+                            <div className="modal__rating">
+                              <h4>
+                                {" "}
+                                {Math.round(title.vote_average * 10)}% match
+                              </h4>
+                            </div>
+                            <h4 className="modal__grid-name">{title.title}</h4>
+                          </div>
+                          <div className="modal__grid-description">
+                            <p>{title.overview.substring(0, 140) + "..."}</p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  }
+
+                  return null;
+                })}
             </div>
           </div>
         </div>
